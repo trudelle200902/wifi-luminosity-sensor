8cc7ecbab282acc59df30a3fc7703504e197659f
Mathieu Trudelle <73758838+trudelle200902@users.noreply.github.com>
2025-01-15 18:36:24 -0500

    added working OTA upload and added info in readme

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-since this is a public github repo the credentials are stored in a file named wifi_config.h that is in the .gitignore for first time use copy wifi_config_template.h and rename it to wifi_config.h and enter your credentials in the file MAKE SURE wigi_config.h IS IN .gitignore BEFORE PUSHING
+# ESP32 Luminosity Sensor for Smart Home
+
+This project implements a luminosity sensor using an ESP32 DevKit. The sensor measures ambient light levels using a photoresistor and sends the data over Wi-Fi, making it suitable for integration into a smart home system. A Python script is also included to demonstrate how to interface with the sensor.
+
+---
+
+## Hardware Components
+1. **ESP32 DevKit**
+2. **Photoresistor**
+3. **10kΩ Resistor**
+4. **Connecting Wires**
+
+### Circuit Diagram
+- **Photoresistor**: 
+  - One leg connected to `5V` of the ESP32.
+  - Other leg connected to `Pin 33` of the ESP32.
+- **10kΩ Resistor**:
+  - Connected between the photoresistor's leg on `Pin 33` and `GND`.
+
+---
+
+## Software Setup
+
+### Prerequisites
+- [PlatformIO](https://platformio.org/) installed in your IDE (e.g., VSCode).
+- Python installed.
+
+### PlatformIO Setup
+1. Clone the repository:
+   ```bash
+   git clone git@github.com:trudelle200902/wifi-luminosity-sensor.git
+   cd wifi-luminosity-sensor
+    ```
+2. Open the project in ypur PlatformIO IDE.
+
+3. Setup your WIFI credentials
+    - Copy wifi_config_template.h in src/
+    - Rename copied file to wifi_config.h
+    - Enter your SSID and password in the new file
+    - VERIFY wifi_config.h IS IN .gitignore IF YOU INTEND TO PUSH
+
+4. First upload
+
+    The code is setup to be uploaded with through wifi with OTA(Over The Air) but for the first upload you will need to do it manually.
+    - Comment the upload_protocol and upload_port line in platformio.ini
+    - Upload the code in the esp32
+    - Take note of the outputed IP address
+    
+    You can now uncomment the line in plaformio.ini and replace the IP address with the one of your esp32 and you will now be able to upload code to the esp32 without being connected to it
+
+5. Python request example
+    
+    An example on how to receive the luminosity from the sensor is shown in the python code python_request_example.py. To receive the luminosity a request message must be sent first to the esp32 then it will send back the luminosity level on a scale of 0-4095. The IP address in the python code might need to be changed for your device.
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,11 @@
 #include <Arduino.h>
 #include "wifi_config.h"
 #include <WiFi.h>
-// #include <ESPmDNS.h>
-// #include <WiFiUdp.h>
-// #include <ArduinoOTA.h>
+#include <ESPmDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 
-#define LED_PIN (32)
 #define PHOTORESISTOR_PIN (33)
-#define LED_TIMEOUT (500)
 #define SYNC_CODE (0xAA55AA55)
 #define MSG_REQUESTED (1)
 
@@ -16,8 +14,6 @@
 #endif
 
 const char* hostname = "esp32";
-uint32_t led_toggle_timestamp = 0;
-uint8_t led_state = LOW;
 WiFiServer server(6666);
 WiFiClient client;
 
@@ -33,58 +29,21 @@ struct __attribute__((packed)) s_message_tx
   uint16_t luminosity;
 };
 
+uint16_t getLuminosity();
+void connectWifi();
+void setupOTA();
+
 void setup() {
   Serial.begin(115200);
 
   // setup IO
-  pinMode(LED_PIN, OUTPUT);
   pinMode(PHOTORESISTOR_PIN, OUTPUT);
 
   // Connect to wifi
-  WiFi.mode(WIFI_STA);
-  WiFi.setHostname(hostname);
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    Serial.println("Connection Failed! Rebooting...");
-    delay(5000);
-    ESP.restart();
-  }
+  connectWifi();
 
-  // if (!MDNS.begin("esp32")) {   // Set the hostname to "esp32.local"
-  //   Serial.println("Error setting up MDNS responder!");
-  //   while(1) {
-  //     delay(1000);
-  //   }
-  // }
-  // Serial.println("mDNS responder started");
-
-  // ArduinoOTA
-  //   .onStart([]() {
-  //     String type;
-  //     if (ArduinoOTA.getCommand() == U_FLASH)
-  //       type = "sketch";
-  //     else // U_SPIFFS
-  //       type = "filesystem";
-
-  //     // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-  //     Serial.println("Start updating " + type);
-  //   })
-  //   .onEnd([]() {
-  //     Serial.println("\nEnd");
-  //   })
-  //   .onProgress([](unsigned int progress, unsigned int total) {
-  //     Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  //   })
-  //   .onError([](ota_error_t error) {
-  //     Serial.printf("Error[%u]: ", error);
-  //     if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-  //     else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-  //     else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-  //     else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-  //     else if (error == OTA_END_ERROR) Serial.println("End Failed");
-  //   });
-
-  // ArduinoOTA.begin();
+  // Setup OTA to be able to update code via WiFi
+  setupOTA();
 
   // Create esp32 server
   server.begin();
@@ -95,15 +54,7 @@ void setup() {
 }
 
 void loop() {
-  // ArduinoOTA.handle();
-
-  // Toggle LED (only for debugging)
-  if(millis()>led_toggle_timestamp){
-    led_state = !led_state;
-    digitalWrite(LED_PIN, led_state);
-    led_toggle_timestamp = millis()+LED_TIMEOUT;
-    Serial.println("toggle");
-  }
+  ArduinoOTA.handle();
 
   if (!client || !client.connected()){ //search client if none are connected
     client = server.available();
@@ -118,11 +69,56 @@ void loop() {
       // build msg
       s_message_tx rsp_msg;
       rsp_msg.sync = SYNC_CODE;
-      rsp_msg.luminosity = analogRead(PHOTORESISTOR_PIN);
+      rsp_msg.luminosity = getLuminosity();
       // send msg
       client.write((uint8_t*)&rsp_msg, sizeof(rsp_msg));
-      Serial.print("message_sent ");
-      Serial.println(rsp_msg.luminosity);
+      // Serial.print("message_sent ");
+      // Serial.println(rsp_msg.luminosity);
     }
   }
+}
+
+uint16_t getLuminosity(){
+  // now just a photoresistor connected to the esp ADC but can be changed in future for more precise measure
+  return analogRead(PHOTORESISTOR_PIN);
+}
+
+void connectWifi(){
+  WiFi.mode(WIFI_STA);
+  WiFi.setHostname(hostname);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+}
+
+void setupOTA(){
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else
+        type = "filesystem";
+
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+
+  ArduinoOTA.begin();
 }
